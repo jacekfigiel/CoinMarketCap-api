05d778d58835933933d9df60892b3d557169a032
jacekfigiel <jacekfigo1988@gmail.com>
2022-09-13 06:52:07 +0100

    fourth commit no classes in finding_crypto.py

--- Test_finding.py
+++ Test_finding.py
@@ -6,7 +6,7 @@ from unittest import mock
 class TestFindingCrypto(unittest.TestCase):
 
     def test_finding_crypto_by_symbol(self, mocked_input):
-        self.cryptos = FindTheWrightCrypto().find_crypto
+        self.cryptos = find_crypto
         mocked_input.side_effect = "ETH"
         self.assertEqual(self.cryptos, "ETH")
 
--- finding_crypto.py
+++ finding_crypto.py
@@ -1,26 +1,19 @@
-from crypto import Crypto
 from json_parser import parse
 
 
-class FindTheWrightCrypto:
-
-    def __init__(self):
-        self.cryptos = parse()
-
-    def find_crypto(self):
-        found_crypto = FindTheWrightCrypto().iterate()
-        if found_crypto:
-            print(found_crypto)
-        else:
-            print("No crypto wit that name.")
-
-    def iterate(self):
-        ask = input("Give a symbol of crypto:\n")
-        for x in self.cryptos:
-            if ask.upper() == x.symbol:
-                return x
-            elif ask.title() == x.name:
-                return x
+def find_crypto():
+    found_crypto = iterate()
+    if found_crypto:
+        print(found_crypto)
+    else:
+        print("No crypto with that name.")
 
 
-FindTheWrightCrypto().find_crypto()
+def iterate():
+    cryptos = parse()
+    ask = input("Give a symbol of crypto:\n")
+    for x in cryptos:
+        if ask.upper() == x.symbol:
+            return x
+        elif ask.title() == x.name:
+             return x
